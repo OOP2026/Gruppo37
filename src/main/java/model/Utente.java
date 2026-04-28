4739d6e136a142c4136f22abf471c992a520ef6f
Nicola000 <ni.maione@studenti.unina.it>
2026-04-28 11:57:12 +0200

    Utente e Studente

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -3,10 +3,16 @@ package model;
 import java.util.ArrayList;
 
 public class Utente {
+    private String nome;
+    private String cognome;
+    private String email;
     private String login;
     private String password;
 
-    public Utente(String login, String password) {
+    public Utente(String nome, String cognome, String email, String login, String password) {
+        this.nome=nome;
+        this.cognome=cognome;
+        this.email=email;
         this.login = login;
         this.password = password;
     }
