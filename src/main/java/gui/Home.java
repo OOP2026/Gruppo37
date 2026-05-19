7466a7e00011767d74235debf2714e47c38ddd94
Nicola000 <ni.maione@studenti.unina.it>
2026-05-19 18:27:12 +0200

    Inizio GUI

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -1,8 +1,28 @@
 package gui;
 
-public class Home {
-	public static void main(String[] args) {
+import javax.swing.*;
 
-	}
+public class Home {
+    private static JFrame frame;
+    //private static Controller controller;;
+    private JPanel panel;
+    private JLabel loginA;
+    private JLabel passwordA;
+    private JLabel passwordR;
+    private JPanel loginR;
+    private JLabel Registrati;
+    private JLabel Accedi;
+    private JTextField TestoLoginA;
+
+    public Home() {}
+
+
+    public static void main(String[] args) {
+        frame= new JFrame("Home");
+        frame.setContentPane(new Home().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 }
