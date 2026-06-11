f2b72a8a0db101585c92cea95663d3edb9bef5d1
Nicola000 <ni.maione@studenti.unina.it>
2026-06-11 12:22:08 +0200

    Classe ConessioneDatabase

--- src/main/java/database_connection/ConnessioneDatabase.java
+++ src/main/java/database_connection/ConnessioneDatabase.java
@@ -1,5 +1,37 @@
 package database_connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnessioneDatabase {
+    private static ConnessioneDatabase istance;
+    public Connection connection=null;
+    private String nome="";
+    private String password="";
+    private String url="";
+    private String driver="org.postgresql.Driver";
+
+    private ConnessioneDatabase() throws SQLException {
+        try{
+            Class.forName(driver);
+            connection=DriverManager.getConnection(url,nome,password);
+        } catch(ClassNotFoundException e){
+            System.out.println("Database Connection Creazione Fallita : " + e.getMessage());
+        }
+    }
+
+    public static ConnessioneDatabase getInstance() throws SQLException {
+        if(istance==null){
+            istance =new ConnessioneDatabase();
+        } else if (istance.connection.isClosed()) {
+            istance = new ConnessioneDatabase();
+        }
+        return istance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 
 }
